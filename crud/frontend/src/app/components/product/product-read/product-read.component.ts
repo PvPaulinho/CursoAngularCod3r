6a8b329cf743f0e2688b9394259b0b9d279c5fb9
Paulo Victor <pvictor36@gmail.com>
2023-11-10 20:09:47 -0300

    Ajustando a exibição da tabela de produtos

--- crud/frontend/src/app/components/product/product-read/product-read.component.ts
+++ crud/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductReadComponent implements OnInit {
 
   products!: Product[];
-  
+  displayedColumn = ['id', 'name', 'price'];
   
   constructor(private productService: ProductService) { }
 
