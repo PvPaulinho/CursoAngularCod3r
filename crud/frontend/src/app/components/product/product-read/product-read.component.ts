4c4d1a59739746b4ba1c9bca03d9e8da085d1ac6
Paulo Victor <pvictor36@gmail.com>
2023-11-17 18:07:25 -0300

    Adicionando coluna de ações

--- crud/frontend/src/app/components/product/product-read/product-read.component.ts
+++ crud/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -10,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductReadComponent implements OnInit {
 
   products!: Product[];
-  displayedColumn = ['id', 'name', 'price'];
+  displayedColumn = ['id', 'name', 'price', 'action'];
   
   constructor(private productService: ProductService) { }
 
